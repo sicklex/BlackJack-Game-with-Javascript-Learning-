d1bedea0a62ce64d386f23628c782d87aa0c9299
sicklex <claydson21@hotmail.com>
2021-08-15 18:39:54 -0300

    Emplementando jogo BlackJack com JavaScript mudanças

---
+++ README.md
@@ -0,0 +1,61 @@
+# Aprendendo JavaScript
+
+Estou aprendendo JavaScript pelo curso do freeCodeCamp e fiz esse projeto  do jogo BlackJack <br>
+<p>No final do modulo eu notei que algumas modificações poderiam ser feitas e a pesar da minha <br>
+limitação no JavaScrip (já que sou iniciante), tentei modificar algumas coisas para ficar um pouco <br>
+mais parecido com um jogo.
+
+## Foram elas:
+
+ - implementei a parte de acrescentar ou descontar o valor que o jogador inicia.
+ - Implementei um bloqueio nos botões de start game e new card quando o jogador fica zerado.
+ - Implemetei um terceiro botão que quando clicado reinicia o jogo com todos os valores iniciais.
+
+## Eu tenho muito a aprender então muitas coisas estarão erradas e acredito ser normal.
+
+
+<br>
+
+# Atualizações futuras
+
+- Por enquanto a unica que consigo pensar é dar a opção de mudança de nome do usuario
+
+
+<br>
+
+### E isso. se tiver alguma dica ou implementação que gostaria de fazer com intuito de me ajudar a melhorar sinta-se a vontade.
+
+<br>
+<br>
+<br>
+<br>
+
+# Learning JavaScript
+
+I'm learning JavaScript through the freeCodeCamp course and I made this BlackJack game project<br>
+At the end of the module I noticed that some changes could be made and despite my<br>
+limitation in JavaScript (since I'm a beginner), i made some little changes so they can be more like a game<br>
+
+
+## What i change:
+- I implemented the part of adding or deducting the value that the player starts.
+- I implemented a lock on the start game and new card buttons when the player is out of money.
+- I implemented a third button that when clicked restarts the game with all initial values.
+
+<br>
+
+I have a lot to learn so a lot of things will be wrong and I believe it's normal.
+
+<br>
+<br>
+
+# Future updates
+
+
+- For now the only thing I can think of is to give the user the option of change the name.
+
+<br>
+<br>
+<br>
+
+### And that's it. If you have any tips or implementations that you would like to do in order to help me improve, feel free.
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let player = {
-    name: "Per",
-    chips: 200
+    name: "Player One",
+    chips: 10
 }
-
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -12,27 +11,32 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
+let youLose = document.getElementById("you-lose")
 
-playerEl.textContent = player.name + ": $" + player.chips
+getInfos()
+
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
     if (randomNumber > 10) {
-        return 10
+        return 10   // to change the queen king and jack values to 10
     } else if (randomNumber === 1) {
-        return 11
+        return 11  // to change the value of Ace card to 11
     } else {
         return randomNumber
     }
 }
 
 function startGame() {
-    isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
-    renderGame()
+    if (player.chips > 0) {
+        hasBlackJack = false 
+        isAlive = true
+        let firstCard = getRandomCard()
+        let secondCard = getRandomCard()
+        cards = [firstCard, secondCard]
+        sum = firstCard + secondCard
+        renderGame()
+    }
 }
 
 function renderGame() {
@@ -47,9 +51,18 @@ function renderGame() {
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
+        player.chips += 10
+        console.log(player.chips)
+        getInfos()
     } else {
         message = "You're out of the game!"
         isAlive = false
+        player.chips -= 10
+        getInfos()
+        if (player.chips ===0) {
+            youLose.textContent = "Sorry you lose, click try again to another chance"
+        }
+        
     }
     messageEl.textContent = message
 }
@@ -61,5 +74,21 @@ function newCard() {
         sum += card
         cards.push(card)
         renderGame()        
+    }
+}
+
+
+function getInfos(){
+    playerEl.textContent = player.name + ": $" + player.chips
+}
+
+
+function outOfChips(){
+    if (player.chips ===0) {
+        player.chips = 10
+        getInfos()
+        sumEl.textContent = "Sum: "
+        cardsEl.textContent = "Cards: "
+        youLose.textContent = null
     }
 }
